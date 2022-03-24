eb50e366bfbe14bb226a079f2dd87dd71ac07084
LucFERRO <luchenri.ferro@gmail.com>
2022-03-24 14:25:52 +0100

    fix darkmode switch

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -90,7 +90,8 @@
             <li>
               <span class="date">2021-2022:</span>
               <p>
-                  Stage de 3 mois chez HoudiniEGM pour réaliser leur projet de MOOC en VueJS et Symfony
+                Stage de 3 mois chez HoudiniEGM pour réaliser leur projet de
+                MOOC en VueJS et Symfony
               </p>
             </li>
             <li>
@@ -175,9 +176,12 @@
           <li>07 83 95 94 63</li>
           <li>luchenri.ferro@gmail.com</li>
         </ul>
-        <!-- <button class="b" onclick="Button()">Dark mode<img</button> -->
         <a id="button" title="Trop de lumière?" @click="Button()">
-          <img id="ON" src="@/assets/img/ONopti.png" alt="OFF"
+          <img
+            id="ON"
+            :src="imgSrc"
+            @click="imgClicked = !imgClicked"
+            alt="Lumière"
         /></a>
       </div>
     </footer>
@@ -187,10 +191,19 @@
 <script>
 export default {
   name: "CV",
+    data () {
+    return {
+      imgClicked: false
+    }
+  },
+  computed: {
+    imgSrc: function () {
+      return this.imgClicked ? require('@/assets/img/OFFopti.png') : require('@/assets/img/ONopti.png')
+    }
+  },
   methods: {
     /* Dark mode */
     Button() {
-      var bulbon = document.getElementById("ON");
       var element = document.getElementsByClassName("right-section");
       for (var right of element) {
         right.classList.toggle("dark-mode");
@@ -198,14 +211,6 @@ export default {
       var element2 = document.getElementsByClassName("left-section");
       for (var left of element2) {
         left.classList.toggle("dark-mode");
-      }
-
-      if (element[0].classList.contains("dark-mode")) {
-        bulbon.setAttribute("src", "assets/img/OFFopti.png");
-        bulbon.setAttribute("title", "Pas assez de lumière?");
-      } else {
-        bulbon.setAttribute("src", "assets/img/ONopti.png");
-        bulbon.setAttribute("title", "Trop de lumière?");
       }
     },
   },
